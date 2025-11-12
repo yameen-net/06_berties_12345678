ecfe73e79f32e40fbd9a7394ba4c06fdd63dcae6
Yameen <myame001@gold.ac.uk>
2025-11-12 17:57:07 +0000

    moving list to main.js

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,4 +2,4 @@
 
 USE berties_books;
 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99), ('The Lean Start Up', 27.99),('Romeo and Julliet', 12.99) ;
