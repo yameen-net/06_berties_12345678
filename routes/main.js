e46f6c7efc7c35fe6540b1b8555cbe03562f7616
Yameen <myame001@gold.ac.uk>
2025-11-12 18:15:48 +0000

    some bbug fixes regarding lists not showing

--- routes/main.js
+++ routes/main.js
@@ -11,16 +11,6 @@ router.get('/about',function(req, res, next){
     res.render('about.ejs')
 });
 
-router.get('/list', function(req, res, next) {
-    let sqlquery = "SELECT * FROM books"; // query database to get all the books
-    // execute sql query
-    db.query(sqlquery, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        res.send(result)
-        });
-});
 
 
 // Export the router object so index.js can access it
