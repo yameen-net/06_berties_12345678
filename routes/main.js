e3e39ae6a0984596a35781ee1fcb01eb3270d09f
Yameen <myame001@gold.ac.uk>
2025-11-18 17:13:14 +0000

    adding home route

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 
 // Handle our routes
-router.get('/',function(req, res, next){
+router.get(['/','/home'],function(req, res, next){
     res.render('index.ejs')
 });
 
